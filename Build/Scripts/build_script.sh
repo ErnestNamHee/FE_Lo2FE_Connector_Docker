9fbe812f009b60aa4b808f2363f96625badb9e66
Ernest NAM HEE <e00360135@china.huawei.com>
2020-08-04 11:13:24 +0200

    04.08.20.6

--- Build/Scripts/build_script.sh
+++ Build/Scripts/build_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -x
 
 DOCKER_BUILD_PATH=`pwd`
-DOCKER_DEPLOYMENT_PATH=`$DOCKER_BUILD_PATH/../../Deployment` 
+DOCKER_PARENT_PATH="$(dirname "$(dirname $DOCKER_BUILD_PATH)")"
+DOCKER_DEPLOYMENT_PATH=`echo $DOCKER_PARENT_PATH/Deployment` 
 
 echo "#####################################################"
 echo "# DOCKER BUILD PATH : $DOCKER_BUILD_PATH"
 echo "# DOCKER DEPLOYMENT PATH : $DOCKER_DEPLOYMENT_PATH"
+echo "# DOCKER PARENT PATH : $DOCKER_PARENT_PATH"
 
